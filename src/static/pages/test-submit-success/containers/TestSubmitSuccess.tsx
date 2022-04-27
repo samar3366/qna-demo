7e15d52551d37a1f032cb103fc6a37f8099f9fd7
samar3366 <sumanthreddypathi3366@gmail.com>
2022-04-27 20:54:00 +0530

    minor

--- src/static/pages/test-submit-success/containers/TestSubmitSuccess.tsx
+++ src/static/pages/test-submit-success/containers/TestSubmitSuccess.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import "./TestSubmitSuccess.scss";
+
 const TestSubmitSuccess: React.FC<any> = (props) => {
-  return <h1>Test Submitted successfully</h1>;
+  return (
+    <div className="success-container">
+      <div className="success-card">
+        <h1>Test Submitted successfully</h1>
+      </div>
+    </div>
+  );
 };
 
 export default TestSubmitSuccess;
